8cbc526f7536ebadf5b1f9425abd4d804e9aecdb
Sean Corcoran <sdc76@cornell.edu>
2019-10-28 22:18:03 -0400

    Alphabetizing

--- db.py
+++ db.py
@@ -6,22 +6,22 @@ db = SQLAlchemy()
 class Announcement(db.Model):
     __table_name = "announcement"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    imageUrl = db.Column(db.String, nullable=False)
-    subject = db.Column(db.String, nullable=False)
     body = db.Column(db.String, nullable=False)
-    ctaText = db.Column(db.String, nullable=False)
     ctaAction = db.Column(db.String, nullable=False)
+    ctaText = db.Column(db.String, nullable=False)
     expirationDate = db.Column(db.Date, nullable=False)
+    imageUrl = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    subject = db.Column(db.String, nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.name,
-            "imageUrl": self.imageUrl,
-            "subject": self.subject,
             "body": self.body,
-            "ctaText": self.ctaText,
             "ctaAction": self.ctaAction,
+            "ctaText": self.ctaText,
             "expirationDate": str(self.expirationDate),
+            "imageUrl": self.imageUrl,
+            "name": self.name,
+            "subject": self.subject,
         }
